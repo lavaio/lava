7f0d05c1534aaa2ee1cc1adf7ae6e3bd0467ed46
dell <574817827@qq.com>
2019-06-20 19:07:10 +0800

    fix bug of exiting when mining & add plotID check when header check & add rpc of getmineraddress

--- src/assember.h
+++ src/assember.h
@@ -16,25 +16,37 @@ public:
 
     ~CPOCBlockAssember();
 
-    bool UpdateDeadline(const uint64_t plotID, const uint64_t nonce, const uint64_t deadline, CScript& script);
+    bool UpdateDeadline(const int height, const uint64_t plotID, const uint64_t nonce, const uint64_t deadline, CScript& script);
 
     void CreateNewBlock(const CScript& scriptPubKeyIn);
 
     void Interrupt();
 
-private:
-    void checkDeadline();
-
+    struct AssemberParams AssemberItems();
+    void SetAssemberItems(const int height, const uint64_t plotID, const uint64_t nonce, const uint64_t deadline, CScript& script);
     void setNull();
 
 private:
+    boost::mutex mtx;
+    void checkDeadline();
+
+private:
     boost::atomic<uint256> genSig;
+    boost::atomic_int height;
     boost::atomic_uint64_t plotID;
     boost::atomic_uint64_t nonce;
     boost::atomic_uint64_t deadline;
     boost::atomic<CScript> scriptPubKeyIn;
     std::shared_ptr<CScheduler> scheduler;
     std::shared_ptr<boost::thread> thread;
+};
+
+struct AssemberParams {
+    int height;
+    uint64_t plotID;
+    uint64_t nonce;
+    uint64_t deadline;
+    CScript scriptPubKeyIn;
 };
 
 #endif // BITCOIN_ASSEMBER_H
