187929afe29ed22631063aba33bb3af81a7b4b8d
eason <zhaoeason@163.com>
2019-06-21 14:15:39 +0800

    open bip16\bip34\bip65

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,11 +65,11 @@ public:
     CMainParams() {
         strNetworkID = "main";
         consensus.nSubsidyHalvingInterval = 210000;
-        consensus.BIP16Exception = uint256S("0x00000000000002dc756eebf4f49723ed8d30cc28a5f108eb94b1ba88ac4f9c22");
-        consensus.BIP34Height = 227931;
-        consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8");
-        consensus.BIP65Height = 388381; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
-        consensus.BIP66Height = 363725; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
+        consensus.BIP16Exception = uint256S("0xdfdcda6aad39077f369ad8b222424fcd0cc24c76b8c20cb5b50477c71f1a652d");
+        consensus.BIP34Height = 0;
+        consensus.BIP34Hash = uint256S("0xdfdcda6aad39077f369ad8b222424fcd0cc24c76b8c20cb5b50477c71f1a652d");
+        consensus.BIP65Height = 0; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
+        consensus.BIP66Height = 0; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
