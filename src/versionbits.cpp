548dd0bab1bec5ffd356945f379d21e2f0b90a89
dell <574817827@qq.com>
2019-07-18 17:34:35 +0800

    remove coinbase witness commitment and validation in checkblock

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -7,7 +7,9 @@
 
 ThresholdState AbstractThresholdConditionChecker::GetStateFor(const CBlockIndex* pindexPrev, const Consensus::Params& params, ThresholdConditionCache& cache) const
 {
-    int nPeriod = Period(params);
+	return ThresholdState::ACTIVE;
+
+	int nPeriod = Period(params);
     int nThreshold = Threshold(params);
     int64_t nTimeStart = BeginTime(params);
     int64_t nTimeTimeout = EndTime(params);
