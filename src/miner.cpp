838b2482a6a6d104150a30c2ca7f7587d835fe54
dell <574817827@qq.com>
2019-07-18 10:24:12 +0800

    remove validation of IsWitnessEnabled of createNewblock

--- src/miner.cpp
+++ src/miner.cpp
@@ -134,7 +134,10 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     // not activated.
     // TODO: replace this with a call to main to assess validity of a mempool
     // transaction (which in most cases can be a no-op).
-    fIncludeWitness = IsWitnessEnabled(pindexPrev, chainparams.GetConsensus());
+    
+	//fIncludeWitness = IsWitnessEnabled(pindexPrev, chainparams.GetConsensus());
+	// TODO: add the IsWitnessEnabled validation of segwit.
+	fIncludeWitness = true;
 
     int nPackagesSelected = 0;
     int nDescendantsUpdated = 0;
