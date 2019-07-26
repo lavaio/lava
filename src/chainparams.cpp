3fdb227bcad02754861f5ee8750a633acb3bbd3e
dell <574817827@qq.com>
2019-07-26 09:43:46 +0800

    modify version

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,7 +124,6 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed1.lavatech.org"); // Pieter Wuille, only supports x1, x5, x9, and xd
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
