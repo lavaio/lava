1caba279cb53060ea660ae3ae6e8ed57138a7d81
dell <574817827@qq.com>
2019-09-03 11:10:23 +0800

    fix error of genesis outpoint's double-spend

--- src/init.cpp
+++ src/init.cpp
@@ -1545,12 +1545,18 @@ bool AppInitMain(InitInterfaces& interfaces)
 
                 // The on-disk coinsdb is now in a good state, create the cache
                 pcoinsTip.reset(new CCoinsViewCache(pcoinscatcher.get()));
-                COutPoint outpoint(chainparams.GenesisBlock().vtx[0]->GetHash(), 0);
-                if (!pcoinsTip->HaveCoin(outpoint)) {
-                    auto txout = chainparams.GenesisBlock().vtx[0]->vout[0];
-                    pcoinsTip->AddCoin(outpoint, Coin(txout, 0, true), true);
-                }
+
                 is_coinsview_empty = fReset || fReindexChainState || pcoinsTip->GetBestBlock().IsNull();
+                if (is_coinsview_empty) {
+                    // record the genesis outpoint in coins DB on disk.
+                    // In older version above, genesis outpoint is recorded in coins DB on-disk.
+                    COutPoint outpoint(chainparams.GenesisBlock().vtx[0]->GetHash(), 0);
+                    if (!pcoinsTip->HaveCoin(outpoint)) {
+                        auto txout = chainparams.GenesisBlock().vtx[0]->vout[0];
+                        pcoinsTip->AddCoin(outpoint, Coin(txout, 0, true), true);
+                    }
+                }
+
                 if (!is_coinsview_empty) {
                     // LoadChainTip sets chainActive based on pcoinsTip's best block
                     if (!LoadChainTip(chainparams)) {
