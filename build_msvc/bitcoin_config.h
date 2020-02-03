893ce99b2bcd548a2edcf0ddaf745a1563093d7e
vent.fang <vent.fang@gmail.com>
2020-02-03 14:09:58 +0800

    enable qrcode

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -392,7 +392,7 @@
 /* #undef USE_DBUS */
 
 /* Define if QR support should be compiled in */
-//#define USE_QRCODE 1
+#define USE_QRCODE 1
 
 /* UPnP support not compiled if undefined, otherwise value (0 or 1) determines
    default state */
