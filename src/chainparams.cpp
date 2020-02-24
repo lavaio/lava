eed3651d3c55420c0b5131c90435d4f55ef67014
dell <574817827@qq.com>
2020-02-24 15:59:37 +0800

    add hardfork height in mainnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -68,6 +68,7 @@ public:
         consensus.BIP34Hash = uint256S("0xdc32ce556d0c3c08d1e770ca772cd2367b46c9b0f4151b276fbf0c7013c4ed0e");
         consensus.BIP65Height = 0; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
         consensus.BIP66Height = 0; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
+        consensus.LVIP05Height = 67584;
         consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
