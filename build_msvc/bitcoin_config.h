cbd90ce2eff8d1af7417a7608aa9ded2657596f3
vent.fang <vent.fang@gmail.com>
2020-02-29 11:10:34 +0800

    fixed qt ui compiling

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -29,7 +29,7 @@
 #define COPYRIGHT_HOLDERS_SUBSTITUTION "Lava Core"
 
 /* Copyright year */
-#define COPYRIGHT_YEAR 2019
+#define COPYRIGHT_YEAR 2020
 
 /* Define to 1 to enable wallet functions */
 #define ENABLE_WALLET 1
